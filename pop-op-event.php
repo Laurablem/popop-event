a131782303ded9de7b2c8e397c28d193c32b6865
Laurablem <eaa24lbvi@students.eaaa.dk>
2025-11-04 12:18:06 +0100

    start på php

---
+++ pop-op-event.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: popop-event
+ * Plugin URI: http://localhost
+ * Description: Popup med datoer til events
+ * Version: 1.0.1
+ * Author: LAURA BLEM VINKLER
+ * Author URI: http://localhost
+ * License: GPL2
+ */
