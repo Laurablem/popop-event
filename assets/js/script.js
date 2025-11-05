1e92a74343f7c0e9501cd0539e2ccca55719ace5
Laurablem <eaa24lbvi@students.eaaa.dk>
2025-11-05 10:45:13 +0100

    js opdate
    vil gerne have en lukning af kalenderen når den ikke er in weiw

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,26 @@ jQuery(document).ready(function($) {
         }
     });
 
+    // Collapse boxes when scrolled out of view
+    $(window).on('scroll', function() {
+        $('.event-wrapper').each(function() {
+            var wrapper = $(this);
+            var container = wrapper.find('.event-boxes-container');
+            
+            // Only check if boxes are currently visible
+            if (container.hasClass('event-visible')) {
+                var wrapperTop = wrapper.offset().top;
+                var wrapperBottom = wrapperTop + wrapper.outerHeight();
+                var scrollTop = $(window).scrollTop();
+                var windowHeight = $(window).height();
+                var windowBottom = scrollTop + windowHeight;
+                
+                // If wrapper is completely out of view (above or below), collapse it
+                if (wrapperBottom < scrollTop || wrapperTop > windowBottom) {
+                    container.removeClass('event-visible').addClass('event-hidden');
+                }
+            }
+        });
+    });
+
 });
